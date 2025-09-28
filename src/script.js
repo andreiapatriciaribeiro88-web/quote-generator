86d5e66dce42440e93f28c8f1d033012c6b4af79
andreiapatriciaribeiro88-web <andreiapatriciaribeiro88@gmail.com>
2025-09-28 15:39:07 +0100

    Added basic form handlingnd added Typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
+function generateQuote(event) {
+  event.preventDefault();
+  new Typewriter("#quote", {
+    strings: "Generating quote",
+    autoStart: true,
+    delay: 10,
+    cursor: "✨",
+  });
+}
 
+let FormElement = document.querySelector("#quote-generator-form");
+FormElement.addEventListener("submit", generateQuote);
