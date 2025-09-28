437110d73dc58240a23630d96390cadbcf8a4fb0
andreiapatriciaribeiro88-web <andreiapatriciaribeiro88@gmail.com>
2025-09-28 18:18:54 +0100

    Remove the console.log

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 function displayQuote(response) {
-  console.log("Quote generatated!");
   new Typewriter("#quote", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,15 +12,13 @@ function generateQuote(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let prompt = `Generate a quote about ${instructionsInput.value}`;
   let context =
-    "You are a seasoned quotes expert who crafts diverse, inspiring, and thought-provoking motivational or wise quotes.  Do NOT include a title or attribution unless the quote is a famous one—if so, mention the author clearly. Always end the quote with '— SheCodes AI' wrapped  a <strong> tag.";
+    "You are a seasoned quotes expert who crafts diverse, inspiring, and thought-provoking motivational or wise quotes.  Do NOT include a title or attribution unless the quote is a famous one—if so, mention the author clearly. Always end the quote with '— SheCodes AI' wrapped INSIDE a <strong> tag.";
   let apiKey = "a9taa49fcab393c9d77od70f76b07b85";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let quoteElement = document.querySelector("#quote");
   quoteElement.classList.remove("hidden");
   quoteElement.innerHTML = `<div class="generating">⏳ Generating a quote for you about ${instructionsInput.value} </div>`;
-
-  console.log("Generating quote.....");
 
   axios.get(apiUrl).then(displayQuote);
 }
